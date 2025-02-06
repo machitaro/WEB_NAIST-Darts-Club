a06153dc86e4323a6975e571205b4405de66f0cb
machitaro <hi01ro27mu@outlook.jp>
2025-02-07 01:25:15 +0900

    Headerの768px未満の余白を調整

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Header = ({onClickToggleMenu}) => {
     return (
         <header className="z-10">
-            <div className="flex mx-auto p-2 px-3 md:px-8 items-center">
+            <div className="flex mx-auto p-2 px-2 md:px-8 items-center">
                 <Link to="/" className="font-bold flex items-center">
                     <span className="text-xl md:text-2xl ml-3">NAISTダーツ倶楽部</span>
                     <div className="w-8 h-8 md:w-10 md:h-10 overflow-hidden ml-1 md:ml-2">
